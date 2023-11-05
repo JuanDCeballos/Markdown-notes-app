fc558e3eb8a2c2f53c26d7969dc7ddc4b1dbc4d4
Juan Ceballos <juandavi789@hotmail.com>
2023-11-04 23:34:41 -0500

    Update the way the note names are displayed, it will take the first line of the note as the title  of it

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -7,7 +7,9 @@ const Sidebar = (props) => {
         }`}
         onClick={() => props.setCurrentNoteId(note.id)}
       >
-        <h4 className='text-snippet'>Note {index + 1}</h4>
+        <h4 className='text-snippet'>
+          {note.body.split('\n')[0].replace('#', '')}
+        </h4>
       </div>
     </div>
   ));
