a89d0b5cd33789d58a50976292d22b46daf3ff07
Juan Ceballos <juandavi789@hotmail.com>
2023-11-14 21:59:06 -0500

    Update the components props, so it now will receive the a temporalNoteText body and a function to update that temporalNoteText

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -3,7 +3,7 @@ import ReactMde from 'react-mde';
 import * as Showdown from 'showdown';
 import 'react-mde/lib/styles/css/react-mde-all.css';
 
-const Editor = ({ currentNote, updateNote }) => {
+const Editor = ({ tempNoteText, setTempNoteText }) => {
   const [selectedTab, setSelectedTab] = useState('write');
 
   const converter = new Showdown.Converter({
@@ -16,8 +16,8 @@ const Editor = ({ currentNote, updateNote }) => {
   return (
     <section className='pane editor'>
       <ReactMde
-        value={currentNote?.body}
-        onChange={updateNote}
+        value={tempNoteText}
+        onChange={setTempNoteText}
         selectedTab={selectedTab}
         onTabChange={setSelectedTab}
         generateMarkdownPreview={(markdown) =>
